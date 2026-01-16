a0d5cfad8d72092d2b990b73fb96409e29640926
OgechaEnock <enockogecha7@gmail.com>
2026-01-16 08:52:22 +0300

    Add Werkzeug WSGI server example in server/werkzeug_app.py

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
 from werkzeug.wrappers import Request, Response
 
 @Request.application
 def application(request):
-    # This prints the IP address of the client making the request
     print(f'This web server is running at {request.remote_addr}')
-    # This sends back a simple HTTP response
     return Response('A WSGI generated this response!')
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(
-        hostname='localhost',  # server runs locally
-        port=5555,             # port number (change if needed)
+        hostname='localhost',
+        port=5555,
         application=application
     )
